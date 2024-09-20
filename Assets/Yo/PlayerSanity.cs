12f8cddff9ec4c505dfb7712c01c9a1aa3024428
Sacol16 <santiago.colonia@uao.edu.co>
2024-09-20 15:22:53 -0500

    Sanity effects

---
+++ Assets/Yo/PlayerSanity.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSanity : MonoBehaviour
+{
+    public float maxSanity = 100f;
+    public float currentSanity;
+    public UnityEngine.UI.Slider sanityBar;
+
+    public SanityEffects sanityEffects;
+
+    private void Start()
+    {
+        currentSanity = maxSanity;
+    }
+
+    public void LoseSanity(float amount)
+    {
+        currentSanity -= amount;
+        currentSanity = Mathf.Clamp(currentSanity, 0, maxSanity);
+
+        // Actualiza la barra de cordura si existe
+        if (sanityBar != null)
+        {
+            sanityBar.value = currentSanity / maxSanity;
+        }
+
+        // Llamar al script de efectos y pasar el porcentaje de cordura
+        if (sanityEffects != null)
+        {
+            float sanityPercentage = currentSanity / maxSanity;
+            sanityEffects.ApplySanityEffects(sanityPercentage);
+        }
+
+        CheckSanityEffects();
+    }
+
+    private void CheckSanityEffects()
+    {
+        if (currentSanity <= 0)
+        {
+            
+            Debug.Log("¡El jugador ha perdido toda la cordura!");
+        }
+        else if (currentSanity < maxSanity / 2)
+        {
+            
+            Debug.Log("La cordura está disminuyendo, efectos activados.");
+        }
+    }
+}
