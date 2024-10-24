d641d3c8d3e0b51f602b9ad0f3d9e862ebf0dc2b
Sacol16 <santiago.colonia@uao.edu.co>
2024-10-24 08:48:58 -0500

    mansion exterior

---
+++ Assets/Mountain Terrain rocks and tree/Script/Camera/CameraControl.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afb85e5d052f2050a24bc19705a17c9bcbfc1ecdccb0e2911bc3da34a7eb7f94
+size 2568
--- Assets/Yo/DisturbingEvent.cs
+++ Assets/Yo/DisturbingEvent.cs
@@ -1,23 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DisturbingEvent : MonoBehaviour
-{
-    public float sanityLoss = 50f; // Cuánto se pierde por presenciar el evento
-    private bool eventTriggered = false; // Para asegurar que el evento no se repita
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") && !eventTriggered)
-        {
-            // Llamar a la función de pérdida de cordura
-            PlayerSanity playerSanity = other.GetComponent<PlayerSanity>();
-            if (playerSanity != null)
-            {
-                playerSanity.LoseSanity(sanityLoss);
-                eventTriggered = true; // Asegurarse de que no se active de nuevo
-            }
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:787e5c90a205c7f976b6bdb0aa57b672788fa0d94cf30c112be0cc636c3b499f
+size 768
