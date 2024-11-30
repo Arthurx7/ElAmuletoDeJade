5a3489e8a318bdafb44aea4cf7483972468e47ae
kelvinguetta1608 <kevinvelasco1608@gmail.com>
2024-11-29 22:31:53 -0500

    Arreglo puertas y efecto disparos

--- Assets/Maca/Scripts/WeaponController.cs
+++ Assets/Maca/Scripts/WeaponController.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.EventSystems; // Importa EventSystems para detectar interacciones con la UI
+using UnityEngine.EventSystems;
 using System.Collections;
 using DialogueEditor;
 
 public class WeaponController : MonoBehaviour
 {
-    public Transform shootSpawn;
+    public Transform shootSpawn; // Boquilla del arma
     public GameObject bulletPrefab;
+    public GameObject muzzleFlashEffect; // Efecto de flash al disparar
 
     public AudioSource audioSource;
     public AudioClip shootSound;
@@ -22,30 +23,27 @@ public class WeaponController : MonoBehaviour
     public float shortReloadTime = 0.5f;
     public float fireRate = 3f;
 
+    public float recoilForce = 4f;
+
     private bool isReloading = false;
     private bool canShoot = true;
 
-    
+    private Vector3 initialPosition;
 
     void Start()
     {
         currentBullets = maxBullets;
-        
-        
+        initialPosition = transform.localPosition;
     }
 
     void Update()
     {
-        // Detener la lógica del arma si la conversación está activa
         if (ConversationManager.Instance.IsConversationActive) return;
-
-        // Si el cursor está desbloqueado (por ejemplo, por la interfaz), no proceses disparos
         if (Cursor.lockState == CursorLockMode.None) return;
-
-        // Si el cursor está sobre la UI, no proceses disparos
         if (EventSystem.current.IsPointerOverGameObject()) return;
 
-        // Detectar disparo con clic izquierdo (Mouse0)
+        transform.localPosition = Vector3.Lerp(transform.localPosition, initialPosition, Time.deltaTime * 5f);
+
         if (Input.GetKeyDown(KeyCode.Mouse0) && canShoot && !isReloading)
         {
             if (currentBullets > 0)
@@ -55,11 +53,9 @@ public class WeaponController : MonoBehaviour
             else
             {
                 Debug.Log("Recarga el arma, no hay balas");
-                
             }
         }
 
-        // Recargar todas las balas cuando se presiona 'R'
         if (Input.GetKeyDown(KeyCode.R) && currentBullets < maxBullets && !isReloading)
         {
             StartCoroutine(FullReload());
@@ -70,7 +66,10 @@ public class WeaponController : MonoBehaviour
     {
         canShoot = false;
 
-        // Raycast desde el centro de la cámara
+        // Crear efecto de flash
+        GameObject flashClone = Instantiate(muzzleFlashEffect, shootSpawn.position, shootSpawn.rotation);
+        Destroy(flashClone, 1f);
+
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
         Vector3 shootDirection;
 
@@ -85,23 +84,30 @@ public class WeaponController : MonoBehaviour
 
         InstantiateBullet(shootDirection);
         currentBullets--;
-        
+
+        AddRecoil();
         audioSource.PlayOneShot(shootSound);
+
         StartCoroutine(ShortReload());
         StartCoroutine(ShootingCooldown());
-
-        
     }
 
     private void InstantiateBullet(Vector3 direction)
     {
-        GameObject bullet = Instantiate(bulletPrefab, shootSpawn.position, Quaternion.LookRotation(direction));
+        GameObject bullet = Instantiate(bulletPrefab, shootSpawn.position, Quaternion.LookRotation(direction) * Quaternion.Euler(0f, 0f, 0f));
+
         Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
 
         if (bulletRb != null)
         {
             bulletRb.velocity = direction * 100f;
         }
+    }
+
+    private void AddRecoil()
+    {
+        transform.localPosition -= new Vector3(0, 0, recoilForce / 50f);
+        transform.localRotation *= Quaternion.Euler(-recoilForce, 0f, 0f);
     }
 
     IEnumerator ShortReload()
@@ -115,12 +121,12 @@ public class WeaponController : MonoBehaviour
     IEnumerator FullReload()
     {
         isReloading = true;
-        
+
         audioSource.PlayOneShot(fullReloadSound);
         Debug.Log("Recargando el arma");
         yield return new WaitForSeconds(fullReloadTime);
         currentBullets = maxBullets;
-        
+
         isReloading = false;
         Debug.Log("Arma recargada");
     }
@@ -130,6 +136,4 @@ public class WeaponController : MonoBehaviour
         yield return new WaitForSeconds(fireRate);
         canShoot = true;
     }
-
-    
 }
