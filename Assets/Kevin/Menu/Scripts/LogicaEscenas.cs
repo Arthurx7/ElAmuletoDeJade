549459b46e185016208dcee3165837b254a148bd
Sacol16 <santiago.colonia@uao.edu.co>
2024-11-30 03:51:53 -0500

    fix brillo e iluminacion

--- Assets/Kevin/Menu/Scripts/LogicaEscenas.cs
+++ Assets/Kevin/Menu/Scripts/LogicaEscenas.cs
@@ -7,13 +7,7 @@ public class LogicaEscenas : MonoBehaviour
     // Start is called before the first frame update
     private void Awake()
     {
-        var noDestruirEntreEscenas = FindObjectsOfType<LogicaEscenas>();
-        if (noDestruirEntreEscenas.Length > 1) 
-        {
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
+       
     }
 
     // Update is called once per frame
